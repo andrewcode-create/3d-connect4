2090302e4fdd0e5960a83682af125808409ea524
Andrew Haas <andrew7haas@gmail.com>
2025-08-23 16:26:22 -0500

    stuff

--- minimax.cpp
+++ minimax.cpp
@@ -1,16 +1,20 @@
-#define MaxBranch = 16
+#define MaxBranch 16
 
 int main() {
 
-
+    return 1;
 }
 
-struct move {
+//must include an invalid state
+struct move_t {
 
+    bool isValid() {
+        return true;
+    }
 };
 
-struct ret {
-    move move;
+struct ret_t {
+    move_t move;
     double score;
 };
 
@@ -20,34 +24,33 @@ enum player {
 
 
 
-struct board {
+struct board_t {
 
 
     // returns an an array of all possible moves for the player's turn, ordered such that the most promising moves are first. 
     // Length is always MaxBranch
-    move* findMoves() {
-
+    move_t* findMoves() {
+        return nullptr;
     }
     
-//dd
     // applies the move to the board
-    void makeMove(move m) {
+    void makeMove(move_t m) {
 
     }
 
     // undos the move
-    void undoMove(move m) {
+    void undoMove(move_t m) {
 
     }
 
     // checks whether a player won. Returns the player.
     player checkWin() {
-
+        return player::NONE;
     }
 
     // assigns a heuristic score to the board, where positive is player 1 and negative is player 2
     double heuristic() {
-        
+        return 0;
     }
 
 
@@ -56,9 +59,42 @@ struct board {
 
 
 // runs minimax
-ret minimax(board& board, player player, int halfMoveNum, int maxhalfMoveNum) {
+ret_t minimax(board_t& board, player player, int halfMoveNum, int maxhalfMoveNum) {
 
+    auto pwin = board.checkWin();
 
+    if (pwin != player::NONE) {
+        ret_t ret;
+        ret.score = (pwin == player::A ? 1 : -1);
+        return ret;
+    }
+
+    ret_t bestmove;
+
+    move_t* moves = board.findMoves();
+
+    for(int i = 0; i < MaxBranch; i++) {
+
+        if (!moves[i].isValid()) {
+            //move is not valid, so went thorugh all valid moves, break.
+            break;
+        }
+
+        board.makeMove(moves[i]);
+        if (player == player::A) {
+            ret_t newmove = minimax(board, player::B, halfMoveNum+1, maxhalfMoveNum);
+            if ( bestmove.move.isValid() || bestmove.score < newmove.score) {
+                bestmove = newmove;
+            }
+        }
+        if (player == player::B) {
+            ret_t newmove = minimax(board, player::A, halfMoveNum+1, maxhalfMoveNum);
+            if ( bestmove.move.isValid() || bestmove.score > newmove.score) {
+                bestmove = newmove;
+            }
+        }
+        return bestmove;
+    }
 
 }
 
